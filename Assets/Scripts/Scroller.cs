a4b089f009f3efeb51e9499c012783a5a9cc663a
Laurent Fourrier <Laurent Fourrier>
2016-07-03 13:58:11 +0200

    Added start/stop key.

--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -3,18 +3,31 @@ using System.Collections;
 
 public class Scroller : MonoBehaviour {
 
+    public GameObject controllerObject;
 	public float scrollSpeed;
 	public float tileWidth;
 
+    private GameController controller;
 	private Vector3 startPosition;
 
+    private float internalClock;
+    private float lastTick;
+
 	void Start () {
+        controller = controllerObject.GetComponent<GameController>();
 		startPosition = transform.position;
+
+        internalClock = 0.0f;
+        lastTick = Time.time;
 	}
 	
 	void Update () {
-		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileWidth);
-		transform.position = startPosition + Vector3.left * newPosition;
-	}
+        if (!controller.isStopped) {
+            internalClock += Time.time - lastTick;
+            float newPosition = Mathf.Repeat(internalClock * scrollSpeed, tileWidth);
+            transform.position = startPosition + Vector3.left * newPosition;
+        }
+        lastTick = Time.time;
+    }
 
 }
