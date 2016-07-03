bb126a92edbaf9fc5f3c2be04e5a8d386ca2c43e
Theomn <theo_menard@hotmail.fr>
2016-07-03 15:24:17 +0200

    added score script (wip)

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    private int timer;
+    private int distance;
+    private GUIText display;
+    public GameController gameController;
+
+	// Use this for initialization
+	void Start () 
+    {
+        display = GetComponent<GUIText>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timer = gameController.timer;
+        distance = gameController.distance;
+        display = "Time left: " + timer + "\n Distance: " + distance;
+
+        timer -= Time.deltaTime;
+	}
+}
